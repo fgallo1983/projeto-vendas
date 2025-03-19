2e795abf2f458e39db54a6f3fdf6fb53d025b592
Felipe <felipe_gallo@yahoo.com>
2025-03-19 06:29:17 -0300

    Atualizando settigns bom base postgree e alowwedhost

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-_-o34chn75m3*4&4)cg6xw=hp98m6-vq#kbcl*bogn(yqe@n6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vendas-098y.onrender.com']
 
 
 # Application definition
@@ -76,11 +78,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('postgresql://felipe:CEu44cVOL4OsDSfW02WaRJgzSy4FDIRa@dpg-cvd8gatumphs73ea4sog-a.oregon-postgres.render.com/db_vendas_bmod'))
 }
 
 
