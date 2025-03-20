7eda7c11d892b534755971d6194d773f057e09b5
Felipe <felipe_gallo@yahoo.com>
2025-03-20 18:26:18 -0300

    Alterando templates redefiniçao da senha

--- vendas/urls.py
+++ vendas/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from .views import CustomPasswordResetView
+
 
 urlpatterns = [
     path('', views.index, name='index'),  # Página inicial (login)
