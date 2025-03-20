7c8a884c50c05f6d11a95ab460bee48ab194a547
Felipe <felipe_gallo@yahoo.com>
2025-03-20 19:07:04 -0300

    Alterando template dir

--- project/settings.py
+++ project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "vendas/templates"],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
